90de04b604049ce7a31cc3bdce5c30f31ddbfd36
Devin <devin.q.rutan@gmail.com>
2017-05-16 09:00:56 -0400

    <Updates>

--- grocery.py
+++ grocery.py
@@ -208,7 +208,7 @@ def main():
 
     input_values = parser.parse_args()
 
-    with open('pghzipcodes.txt') as f:
+    with open('nyczipcodes.txt') as f:
         content = f.readlines()
     # you may also want to remove whitespace characters like `\n` at the end of each line
     content = [x.strip() for x in content]
